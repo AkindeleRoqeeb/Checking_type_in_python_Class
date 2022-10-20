0b5645e6eb3c2d1415771fb83caa3e3514c6112e
roqeebakindele@gmail.com <roqeebakindele@gmail.com>
2022-10-20 02:56:17 +0100

    data-type

---
+++ start.py
@@ -0,0 +1,17 @@
+class item():
+    pass
+
+item1 = item()
+
+item1.name = 'laptop'
+item1.price = 300
+item1.quality = 634
+item1.quantity = 532
+item1.location = 'Nigeria'
+
+print(type(item1))
+print(type(item1.name))
+print(type(item1.price))
+print(type(item1.quality))
+print(type(item1.quantity))
+print(type(item1.location))
